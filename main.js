d7264ddaa01fd38b882c1db2edfde83a396d06e8
Michael Obeng <michaelldninc@gmail.com>
2019-08-26 21:13:42 +0100

    changed images to pngs and added preloader

---
+++ main.js
@@ -0,0 +1,14 @@
+let loader = document.querySelector('.loader');
+let main = document.querySelector('main');
+
+function load() {
+    setTimeout(() => {
+        loader.style.opacity = '0';
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(() => (main.style.opacity = 1), 50);
+    }, 4000);
+}
+
+load();
