3c116d768b0ae90496c8815a7581ffd61693d9f0
Michael Obeng <michaelldninc@gmail.com>
2019-08-26 21:31:36 +0100

    increased timeout and changed string to integer

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let main = document.querySelector('main');
 
 function load() {
     setTimeout(() => {
-        loader.style.opacity = '0';
+        loader.style.opacity = 0;
         loader.style.display = 'none';
 
         main.style.display = 'block';
         setTimeout(() => (main.style.opacity = 1), 50);
-    }, 4000);
+    }, 5000);
 }
 
 load();
